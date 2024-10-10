fe4b7e7ecc62d5cfb9f8763680f2abec240ed666
Addison Snyder <addison.snyder@va.gov>
2024-10-10 12:41:56 -0400

    Create setup.ps1
    Script downloads and configures Java, Maven, Eclipse, and Git. It can add VA CA cert to the Java installation, and sets appropriate PATH and other environmental variables. Made to be run on GFEs primarily, but should work for any Windows machines.

---
+++ setup.ps1
@@ -0,0 +1,158 @@
+
+# Set the target directory and URLs
+$targetDir = "$HOME\HATS"
+$7zipUrl = "https://www.7-zip.org/a/7zr.exe"
+$gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.1/PortableGit-2.47.0-64-bit.7z.exe"
+$downloads = @{
+    "java"   = "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jdk_x64_windows_hotspot_11.0.9.1_1.zip"
+    "maven"  = "https://downloads.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip"
+    "eclipse"= "https://download.eclipse.org/technology/epp/downloads/release/2024-12/M1/eclipse-jee-2024-12-M1-win32-x86_64.zip"
+}
+
+# Function to download a file
+function Download-File-And-Extract {
+    param (
+        [string]$url,
+        [string]$path,
+        [string]$app
+    )
+
+    if (Test-Path $path) {
+        $overwrite = Read-Host "File $path already exists. Overwrite? (Y/N)"
+        if ($overwrite -ne 'Y') {
+            Write-Host "Skipping download of $url"
+            return
+        }
+    }
+    
+    Write-Host "Downloading $url..."
+    Invoke-WebRequest -Uri $url -OutFile $path
+    Write-Host "Extracting $filePath..."
+    Unzip-File -zipPath $filePath -extractPath "$targetDir\$app"
+}
+
+# Function to unzip files
+function Unzip-File {
+    param (
+        [string]$zipPath,
+        [string]$extractPath
+    )
+    try {
+        Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
+        Write-Host "Unzipped: $zipPath to $extractPath"
+    } catch {
+        Write-Host "Failed to unzip: $zipPath"
+    }
+}
+
+# Function to unzip files
+function Add-To-Path-From-Filename {
+    param (
+        [string]$searchDirectory,
+        [string]$filename
+    )
+
+    # Find all directories containing the specified file
+    $directories = Get-ChildItem -Path $searchDirectory -Recurse -Filter $fileName | Select-Object -ExpandProperty DirectoryName -Unique
+
+    foreach ($directory in $directories) {
+        # Get the current user PATH
+        $currentPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
+        # Check if the directory is already in PATH
+        if (!($currentPath -contains $directory)) {
+            # Add the directory to PATH
+            $newPath = "$currentPath;$directory"
+            #[System.Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+            [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$directory", [EnvironmentVariableTarget]::User)
+            #Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+            Write-Host "Added $directory to user PATH."
+        } else {
+            Write-Host "$directory is already in user PATH."
+        }
+    }
+
+    if (-not $directories) {
+        Write-Host "No directories found containing the file '$fileName'."
+    }
+}
+
+# Create target directory if it doesn't exist
+if (-not (Test-Path $targetDir)) {
+    New-Item -ItemType Directory -Path $targetDir
+}
+
+# Download/Extract the portable (zipped) applications
+foreach ($app in $downloads.Keys) {
+    $filePath = Join-Path -Path $targetDir -ChildPath "$app.zip"
+    Download-File-And-Extract -url $downloads[$app] -app $app -path $filePath
+}
+
+# Download 7-Zip if it's not already installed
+$sevenZipPath = "$HOME\HATS\7zr.exe"
+if (-not (Test-Path $sevenZipPath)) {
+    Write-Host "Downloading portable 7-Zip (only good for 7z files)..."
+    Invoke-WebRequest -Uri $7zipUrl -OutFile "$HOME\HATS\7zr.exe"
+} else {
+    Write-Host "7zr.exe already downloaded"
+}
+
+if (!(Test-Path "$targetDir\git.zip")) {
+    Write-Host "Downloading $gitUrl..."
+    Invoke-WebRequest -Uri $gitUrl -OutFile (Join-Path -Path $targetDir -ChildPath "git.zip")
+    #Unzip git
+    & "$sevenZipPath" x "$HOME\HATS\git.zip" -o"$HOME\HATS\git" -y
+} else {
+    Write-Host "Git already downloaded. Skipping.."
+}
+
+Add-To-Path-From-Filename -searchDirectory "$HOME\HATS\java" -filename "java.exe"
+Add-To-Path-From-Filename -searchDirectory "$HOME\HATS\maven" -filename "mvn"
+Add-To-Path-From-Filename -searchDirectory "$HOME\HATS\git" -filename "git.exe"
+
+#use the LATEST directory inside the java folder. Hacky but it'll work..
+$JavaHome = Get-ChildItem -Path "$HOME\HATS\java" -Directory | Sort-Object CreationTime -Descending | Select-Object -First 1
+
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $JavaHome, "User")
+[Environment]::SetEnvironmentVariable("JAVA_HOME", $JavaHome.FullName, [EnvironmentVariableTarget]::User)
+Write-Host "Added $JavaHome as JAVA_HOME."
+
+# Keep the PowerShell window open
+Write-Host "All software installation tasks completed. Press Enter to continue..."
+[void][System.Console]::ReadLine()
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+$env:JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", [System.EnvironmentVariableTarget]::User)
+
+#TODO - add cacerts to JDK installation
+
+$overwrite = Read-Host "If you use the VA VPN, public certs are required to interact with certain resources. Download and install? (Y/N)"
+if ($overwrite -eq 'Y') {
+    Write-Host "Downloading VA Cert..."
+    Invoke-WebRequest -Uri "https://department-of-veterans-affairs.github.io/github-handbook/guides/security/VA-Internal-S2-RCA1-v1.pem" -OutFile "$targetDir\VA-Internal-S2-RCA1-v1.pem"
+    Write-Host "Adding CA cert to cacerts files..."
+    # Find all directories containing the specified file
+    $cacertsFiles = Get-ChildItem -Path $targetDir -Recurse -Filter "cacerts" | Select-Object -ExpandProperty DirectoryName -Unique
+    foreach ($cacertFile in $cacertsFiles) {
+        Write-Host "Adding cert to $cacertFile"
+        keytool.exe -import -alias va-certificate-authority -file "$targetDir\VA-Internal-S2-RCA1-v1.pem" -keystore $cacertFile -storepass changeit -noprompt
+    }
+}
+
+mvn -v
+git -v
+
+$enterGithubCredentials = Read-Host "Enter Github Credentials? (Y/N)"
+if ($enterGithubCredentials -eq 'Y') {
+    $GithubToken = Read-Host -AsSecureString -Prompt "Please navigate to https://github.com/settings/tokens  and generate a new Classic Token with 'repo' and 'write:packages' scopes. Paste the generated key here"
+    $GithubUsername = Read-Host -Prompt "Enter your Github Username (Typically your email)"
+
+    Write-Host "Adding Github Username/Password to environmental variables."
+    [Environment]::SetEnvironmentVariable("GITHUB_USR", $GithubUsername, [EnvironmentVariableTarget]::User)
+    [Environment]::SetEnvironmentVariable("GITHUB_PSW", $GithubToken, [EnvironmentVariableTarget]::User)
+}
+# Keep the PowerShell window open
+[void][System.Console]::ReadLine()
+
+cd $targetDir
+
+git clone https://github.com/department-of-veterans-affairs/hats-cucumber-framework.git
